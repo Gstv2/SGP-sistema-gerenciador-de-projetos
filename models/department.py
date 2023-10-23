69cb40fac0e0e601771a2e87789ac1ec42938ab6
Gstv2 <nerysilva2006@gmail.com>
2023-10-23 14:01:42 -0300

    versão aprimorada

--- models/department.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column , Integer , String 
-from . base import Base
-
-
-class Department(Base):
-    __tablename__ = 'departments'
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False )
---
+++ models/kanbanboard.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from .base import Base
+
+class KanbanBoard(Base):
+    __tablename__ = 'KanbanBoards'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=True)
+    description = Column(Text)
+    user_email = Column(String(100), ForeignKey('user.email'), nullable=False)
+
+    # Relacionamento com User e KanbanCard
+    user = relationship("User", back_populates="kanbanboards")
+    tarefas = relationship("KanbanCard", back_populates="kanbanboard")
